925ed0e0cde4f82903b380d26084cea420ffe585
Josedlca <jose.dlcruz.ayala@gmail.com>
2019-10-07 18:59:29 -0500

    add

--- src/components/InputData.vue
+++ src/components/InputData.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="w-8/12 h-8 mx-auto flex items-center">
-		<input class="border-red-400 border w-8/12 outline-none h-full" type="text" v-model="inData">
-		<button class="w-4/12 bg-red-400 hover:bg-red-600 border border-red-400 h-full text-white font-bold" @click="moreTasks(inData)">Archive</button>
+	<div class="w-8/12 h-10 mx-auto flex items-center mb-6">
+		<input class="outline-none border border-red-400 p-2 rounded-l-lg w-9/12 h-full" type="text" v-model="inData">
+		<button class="w-3/12 bg-red-400 rounded-r-lg focus:outline-none  border border-red-400 h-full text-white font-bold outline-none" @click="moreTasks(inData)">Archive</button>
 	</div>
 </template>
 
--- src/components/ShowInpData.vue
+++ src/components/ShowInpData.vue
@@ -1,10 +1,15 @@
 <template>
-	<div class="w-8/12 mx-auto">
-		<div class="py-2" v-for="(item, index) of showStore" :key="item.id">
-			<span class="text-red-600">{{index}}</span> 
-			<p class="inline">{{item.task}}</p>  
-			<input type="checkbox" v-model="item.taskState">
-			<button @click="deleteTask(index)">X</button>
+	<div class="w-8/12 h-10 mx-auto">
+		<div class="py-1 h-full border border-red-400 rounded-lg flex items-center justify-between mb-3" v-for="(item, index) of showStore" :key="item.id">
+			<p class="inline pl-2 ">{{item.task}}</p>  
+			<div class="">
+				<input  type="checkbox" v-model="item.taskState">
+				<button class="outline-none"  @click="deleteTask(index)">
+					<svg class="w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+						<path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"/>
+					</svg>
+				</button>
+			</div>
 		</div>
 		<button @click="groupalDelete">delete</button>
 	</div>
